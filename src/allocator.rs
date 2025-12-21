d1a480f1480bfdb65da1fdff6aafdfe9066e8e20
pullriku <riku3dno.1@gmail.com>
2025-12-21 14:09:52 +0900

    refactor: UFCS

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,9 +1,19 @@
 use core::{alloc::Layout, ptr::NonNull};
 
+pub mod bump;
+pub mod free_list;
+
 pub trait MutAllocator {
     unsafe fn alloc(&mut self, layout: Layout) -> Option<NonNull<[u8]>>;
     unsafe fn dealloc(&mut self, ptr: NonNull<u8>, layout: Layout);
 }
 
-pub mod bump;
-pub mod free_list;
+impl<A: MutAllocator + ?Sized> MutAllocator for &mut A {
+    unsafe fn alloc(&mut self, layout: Layout) -> Option<NonNull<[u8]>> {
+        unsafe { <A as MutAllocator>::alloc(&mut **self, layout) }
+    }
+
+    unsafe fn dealloc(&mut self, ptr: NonNull<u8>, layout: Layout) {
+        unsafe { <A as MutAllocator>::dealloc(&mut **self, ptr, layout) }
+    }
+}
